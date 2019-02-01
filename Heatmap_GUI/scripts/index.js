69dad62927eb20528c3a5f435e072ca42929f84a
Paul Brière <paul.briere94@gmail.com>
2019-02-01 11:56:26 -0500

    instant strip update when heat range parameters are chaged

--- Heatmap_GUI/scripts/index.js
+++ Heatmap_GUI/scripts/index.js
@@ -6,6 +6,7 @@ MAX_AMP = 10;
 MIN_AMP = 0;
 
 function initMap(sensorCount){
+    amp_data = []
     var map = document.getElementById("map");
     map.innerHTML = '';
     console.log(map);
@@ -17,6 +18,7 @@ function initMap(sensorCount){
                '<div class="sensor"></div>'+
                '<a class="indicator">-</a>'+
              '</div>');
+            amp_data.push(0);
       })();
       updateSensor(i, i)
     }
@@ -29,10 +31,19 @@ function updateSensor(sensorIdx, ampValue) {
 
     sensors[sensorIdx].style.background = heatMapColorforValue(ampValue);
     indicator[sensorIdx].text = "" + ampValue + " A";
+    amp_data[sensorIdx] = ampValue
+}
+
+function refreshStrip() {
+
+    for(var i = 0; i < amp_data.length; i++) {
+      updateSensor(i, amp_data[i]);
+    }
 }
 
 function heatMapColorforValue(value){
-  var h = (1.0 - (value)/MAX_AMP) * 240
+  var h = 
+    (1.0 - Math.min(Math.max(value-MIN_AMP,0)/(MAX_AMP-MIN_AMP),1)) * 240
   return "hsl(" + h + ", 100%, 50%)";
 }
 
@@ -40,10 +51,12 @@ var maxHeatRange = document.getElementById("maxHeatRange");
 maxHeatRange.value = MAX_AMP;
 maxHeatRange.oninput = function () {
     MAX_AMP = maxHeatRange.value;
+    refreshStrip()
 }
 
 var minHeatRange = document.getElementById("minHeatRange");
 minHeatRange.value = MIN_AMP;
 minHeatRange.oninput = function () {
     MIN_AMP = minHeatRange.value;
+    refreshStrip()
 }
