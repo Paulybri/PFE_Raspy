71de63314723916bc3efd37b27bafb9d37693aab
Paul Brière <paul.briere94@gmail.com>
2019-02-17 21:26:44 -0500

    Added sql

--- Server/app.py
+++ Server/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import sqlite3
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +8,17 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
+DATABASE = '/database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
