4b569f753f8106a302bbc52b0b37b9b3e41cb2d5
Paul Brière <paul.briere94@gmail.com>
2019-03-08 09:25:39 -0500

    Arduino tweak

--- arduino/serial_test/serial_test.ino
+++ arduino/serial_test/serial_test.ino
@@ -9,11 +9,6 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  //timestamp simulation counter
-  timeStampCounter++;
-  if (timeStampCounter % 1000 == 0){
-    timeStamp++;
-  }
    // send data only when you receive data:
   if (Serial.available() > 0) {
           // read the incoming byte:
@@ -29,24 +24,15 @@ void loop() {
             case 0x44:              
               ampValue = 40;            
           }
-          uint8_t ts[4];
-          ts[0] = timeStamp;
-          ts[1] = timeStamp >>  8;
-          ts[2] = timeStamp >> 16;
-          ts[3] = timeStamp >> 24;
           
           Serial.write(incomingByte);
           
-          Serial.write(ts, 4);
           Serial.write(ampValue);
 
-          Serial.write(ts, 4);
           Serial.write(ampValue+1);
 
-          Serial.write(ts, 4);
           Serial.write(ampValue+2);
 
-          Serial.write(ts, 4);
           Serial.write(ampValue+3);
 
 
