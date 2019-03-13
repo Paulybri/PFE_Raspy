f77166c68ccdbc52e815b9929434381254d6cd9f
Paulybri <paul.briere94@gmail.com>
2019-03-13 12:29:16 -0400

    communication with uC functionnal

--- Server/app.py
+++ Server/app.py
@@ -7,6 +7,7 @@ import datetime
 import struct
 import threading
 import atexit
+import math
 
 # SQLITE FUNCTIONS ----------------------
 
@@ -19,7 +20,7 @@ def get_db():
     return db
 	
 def data_entry(db,timestamp,uC,sensor,value):
-	db.cursor().execute('INSERT INTO amp (timestamp,uC,sensor,value) VALUES(?, ?, ?, ?)',(timestamp,uC,sensor,value))
+	db.cursor().execute('INSERT INTO amp (timestamp,uC,sensor,current) VALUES(?, ?, ?, ?)',(timestamp,uC,sensor,value))
 	db.commit()
 
 def read_all_db(db):
@@ -29,38 +30,48 @@ def read_all_db(db):
 	return data
 
 # COMMUNICATION FUNCTIONS --------------
-port = '/dev/ttyAMA0' #GPIO UART
+#port = '/dev/ttyAMA0' #GPIO UART
 #port = '/dev/ttyACM0' #USB PORT
-ser = serial.Serial(port, 9600, timeout = 1, writeTimeout = 1)
+port = '/dev/ttyUSB0' #USB PORT
 
-ucAddresses = [0x41,0x42,0x43,0x44]
+ucAddresses = [0x41]
 
 def uc_probe_and_store(ucAddress):
-	ser.reset_input_buffer()
+	ser = serial.Serial(
+	port=port,\
+	baudrate=9600,\
+	timeout=1,\
+	parity=serial.PARITY_NONE,\
+	stopbits=serial.STOPBITS_ONE,\
+	bytesize=serial.EIGHTBITS)
+	
 	ser.write('' + struct.pack('!B',ucAddress))
 	response  = ser.read(1)
 	if response == ('' + struct.pack('!B',ucAddress)) :
-		for i in range(1,5):
+		print('Recieved acknowledgment from uC')
+		for i in range(1,2):#(1,5):
 			timeStamp = datetime.datetime.now()
-			ampValue = ord(ser.read(1)) 
-			
-			data_entry(get_db(),timeStamp, ucAddress, i, ampValue)
-			'''print('sensor #',i)
+			variance = struct.unpack('!H',ser.read(2))
+			standDev = math.sqrt(variance[0])
+			current = (standDev-17.252)/2.5464
+			data_entry(get_db(),timeStamp, ucAddress, i, current)
+			print('sensor #',i)
 			print('timeStamp: ', timeStamp)
-			print('ampValue: ', ampValue)
-			print('\n')'''
+			print('standard deviation: ', standDev)
+			print('current: ',current)
+			print('\n')
+				
 	else:
-		print("uC didn't send correct response")
-		print("Expected :",ucAddress)
-		print("Recieved :", hex(ord(response)))
-
-def read_serial_and_print():
-	#ser.reset_input_buffer()
-	response  = ser.read(4)
-	print('Data recieved: ', response);
+		if len(response) > 0:
+			print("uC didn't send correct response")
+			print("Expected :",ucAddress)
+			print("Recieved :", hex(ord(response)))
+		else:
+			print("Delay expired, no response")
+	ser.close()
 
 # FLASK APP ----------------------------
-POOL_TIME = 5 #Seconds
+POOL_TIME = 2 #Seconds
 
 # lock to control access to variable
 dataLock = threading.Lock()
@@ -78,11 +89,10 @@ def probe():
 	with dataLock and app.app_context():
 
 		db = get_db()
-		db.cursor().execute('CREATE TABLE IF NOT EXISTS amp(timestamp STRING, uc INT, sensor INT, value REAL)')
+		db.cursor().execute('CREATE TABLE IF NOT EXISTS amp(timestamp STRING, uc INT, sensor INT, current REAL)')
 		print('Probing uCs')
 		for i in ucAddresses:
 			uc_probe_and_store(i)
-			#read_serial_and_print()
 		
 	# Set the next thread to happen
 	probingThread = threading.Timer(POOL_TIME, probe, ())
@@ -99,7 +109,7 @@ def index():
 	return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+	app.run(debug=False, host='0.0.0.0')
     
 @app.teardown_appcontext
 def close_connection(exception):
